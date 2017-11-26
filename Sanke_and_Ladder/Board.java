98b1c45ab84238c361386316f88531ef33558720
unneeks <unneeks@gmail.com>
2017-11-26 10:47:46 +0530

    comments

--- Sanke_and_Ladder/Board.java
+++ Sanke_and_Ladder/Board.java
@@ -1,8 +1,15 @@
 
 public class Board {
-	
+
+//  cells[]
+// at this time - this class does not have any substance
+// it is only have only one behavior called print. which is actually not  natural behavior of the Board
+//  i like to have a board which can be set up in different combinations of snake and ladders
+// each time I initialise a board I would like to have different set of snake and ladders
+
+
 	public void printBoard() {
-		
+
 		System.out.println("Lets start the game");
 		System.out.println("=================================================================================");
 
